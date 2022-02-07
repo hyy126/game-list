92e79c557a2021378f93f958c922c57eaa07bf74
hyy126 <375318939@qq.com>
2022-02-07 11:48:20 +0800

    feat: router route path

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [{
-    path: '/minesweeper',
+    path: '/', // minesweeper
     name: 'minesweeper',
     component: () => import('../views/mine-sweeper/index.vue'),
   }]
