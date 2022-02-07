1d89d6544a92bc151261439f0321028392cc30f9
hyy126 <375318939@qq.com>
2022-02-07 11:46:06 +0800

    feat: new build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `https://hyy126.github.io/game-list/`,
+  base: `https://hyy126.github.io/game-list/dist`,
   plugins: [vue()]
 })
